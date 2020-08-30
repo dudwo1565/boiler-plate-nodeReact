eb0d653ee18c1138ed34ae2470af8669ae320e05
dudwo1565 <dudwo1565@gmail.com>
2020-08-31 02:08:44 +0900

    노드 리액트 기초강의 32

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -3,10 +3,15 @@ import axios from "axios";
 
 function LandingPage() {
   useEffect(() => {
-    axios.get("http://localhost:5000/api/hello")
+    axios.get("/api/hello")
     .then(response => console.log(response.data))
   }, []);
-  return <div>LandingPage - 랜딩페이지</div>;
+  return <div style={{
+    display: 'flex', justifyContent: 'center', alignItems:'center',
+    width:'100%', height: '100vh'
+  }}>
+    <h2>시작 페이지</h2>
+  </div>;
 }
 
 export default LandingPage;
